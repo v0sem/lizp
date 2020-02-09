0a344ef175cddba3c66d8432355c976bd03ceafd
v0sem <progpabsanch@gmail.com>
2020-02-09 18:49:35 +0100

    Newton working with stupid warning

--- p1/code.lisp
+++ p1/code.lisp
@@ -2,19 +2,23 @@
 
 
 (defun newton (f df-dx max-iter x0 &optional (tol-abs 0.0001))
-  "Zero of a function using the Newton-Raphson method
+	"Zero of a function using the Newton-Raphson method
 
 
-    INPUT:  f:        function whose zero we wish to find
-            df-dx:    derivative of f
-            max-iter: maximum number of iterations 
-            x0:       initial estimation of the zero (seed)
-            tol-abs:  tolerance for convergence
+		INPUT:  f:        function whose zero we wish to find
+						df-dx:    derivative of f
+						max-iter: maximum number of iterations 
+						x0:       initial estimation of the zero (seed)
+						tol-abs:  tolerance for convergence
 
 
-    OUTPUT: estimation of the zero of f, NIL if not converged"
-  )
-
+		OUTPUT: estimation of the zero of f, NIL if not converged"
+    (if ( = max-iter 0) nil
+        (let ((x (- x0 (/ (funcall f x0) (funcall df-dx x0)))))))
+    (if (< (abs(- x x0)) tol-abs)
+		x0
+		(newton f df-dx (- max-iter 1) x tol-abs)))
+    
 
 
 
@@ -22,18 +26,18 @@
 
 
 (defun newton-all (f df-dx max-iter seeds &optional (tol-abs 0.0001))
-  "Zeros of a function using the Newton-Raphson method
+	"Zeros of a function using the Newton-Raphson method
 
 
-    INPUT:  f:        function whose zero we wish to find
-            df-dx:    derivative of f
-            max-iter: maximum number of iterations 
-            seeds:    list of initial estimations of the zeros 
-            tol-abs:  tolerance for convergence
+		INPUT:  f:        function whose zero we wish to find
+						df-dx:    derivative of f
+						max-iter: maximum number of iterations 
+						seeds:    list of initial estimations of the zeros 
+						tol-abs:  tolerance for convergence
 
 
-    OUTPUT: list of estimations of the zeros of f"
-  )
+		OUTPUT: list of estimations of the zeros of f"
+	)
 
 
 
@@ -44,49 +48,49 @@
 
 
 (defun combine-elt-lst (elt lst)
-  "Combines an element with all the elements of a list
+	"Combines an element with all the elements of a list
 
 
-    INPUT:  elt: element 
-            lst: list 
+		INPUT:  elt: element 
+						lst: list 
 
 
-    OUTPUT: list of pairs, such that 
-               the first element of the pair is elt. 
-               the second element is an element from lst"
+		OUTPUT: list of pairs, such that 
+							 the first element of the pair is elt. 
+							 the second element is an element from lst"
  ) 
-  
+	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun combine-list-of-lsts (lolsts)
-  "Combinations of N elements, each of wich
+	"Combinations of N elements, each of wich
 
 
-   INPUT:  lstolsts: list of N sublists (list1 ... listN)
+	 INPUT:  lstolsts: list of N sublists (list1 ... listN)
 
 
-   OUTPUT: list of sublists of N elements, such that in each 
-           sublist the first element is from list1
-                 the second element is from list 2
-                 ...
-                 the Nth element is from list N"
-  
-  )
+	 OUTPUT: list of sublists of N elements, such that in each 
+					 sublist the first element is from list1
+								 the second element is from list 2
+								 ...
+								 the Nth element is from list N"
+	
+	)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun scalar-product (x y)
-  "Calculates the scalar product of two vectors
+	"Calculates the scalar product of two vectors
  
-   INPUT:  x: vector, represented as a list
-           y: vector, represented as a list
+	 INPUT:  x: vector, represented as a list
+					 y: vector, represented as a list
  
-   OUTPUT: scalar product between x and y
+	 OUTPUT: scalar product between x and y
 
 
-   NOTES: 
-        * Implemented with mapcar"
-    (reduce #'+ (mapcar #'* x y))
-  )
+	 NOTES: 
+				* Implemented with mapcar"
+		(reduce #'+ (mapcar #'* x y))
+	)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -94,12 +98,12 @@
 
 
 (defun euclidean-norm (x)
-  "Calculates the euclidean (l2) norm of a vector
-   
-    INPUT:  x: vector, represented as a list
+	"Calculates the euclidean (l2) norm of a vector
+	 
+		INPUT:  x: vector, represented as a list
 
 
-    OUTPUT: euclidean norm of x"
+		OUTPUT: euclidean norm of x"
 )
 
 
@@ -111,81 +115,81 @@ ________________
 
 
 (defun euclidean-distance (x y) 
-  "Calculates the euclidean (l2) distance between two vectors
+	"Calculates the euclidean (l2) distance between two vectors
  
-    INPUT: x: vector, represented as a list
-           y: vector, represented as a list
+		INPUT: x: vector, represented as a list
+					 y: vector, represented as a list
 
 
-    OUTPUT: euclidean distance between x and y"
-  )
+		OUTPUT: euclidean distance between x and y"
+	)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (defun cosine-similarity (x y) 
-  "Calculates the cosine similarity between two vectors
+	"Calculates the cosine similarity between two vectors
 
 
-    INPUT:  x: vector, representad as a list
-            y: vector, representad as a list
+		INPUT:  x: vector, representad as a list
+						y: vector, representad as a list
 
 
-    OUTPUT: cosine similarity between x and y
+		OUTPUT: cosine similarity between x and y
 
 
-    NOTES: 
-       * Evaluates to NIL (not defined)
-         if at least one of the vectors has zero norm.
-       * The two vectors are assumed to have the same length"
-  )
+		NOTES: 
+			 * Evaluates to NIL (not defined)
+				 if at least one of the vectors has zero norm.
+			 * The two vectors are assumed to have the same length"
+	)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defun angular-distance (x y) 
-  "Calculates the angular distance between two vectors
+	"Calculates the angular distance between two vectors
 
 
-   INPUT:  x: vector, representad as a list
-           y: vector, representad as a list
+	 INPUT:  x: vector, representad as a list
+					 y: vector, representad as a list
 
 
-   OUTPUT: cosine similarity between x and y
+	 OUTPUT: cosine similarity between x and y
 
 
-   NOTES: 
-      * Evaluates to NIL (not well defined)
-        if at least one of the vectors has zero norm.
-      * The two vectors are assumed to have the same length"
+	 NOTES: 
+			* Evaluates to NIL (not well defined)
+				if at least one of the vectors has zero norm.
+			* The two vectors are assumed to have the same length"
 
 
-  )
+	)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; select-vectors
 
 
 (defun select-vectors (lst-vectors test-vector similarity-fn &optional (threshold 0))
-    "Selects from a list the vectors whose similarity to a 
-     test vector is above a specified threshold. 
-     The resulting list is ordered according to this similarity.
+		"Selects from a list the vectors whose similarity to a 
+		 test vector is above a specified threshold. 
+		 The resulting list is ordered according to this similarity.
  
-     INPUT:  lst-vectors:   list of vectors
-             test-vector:   test vector, representad as a list
-             similarity-fn: reference to a similarity function
-             threshold:     similarity threshold (default 0)
-      
-     OUTPUT: list of pairs. Each pair is a list with
-             a vector and a similarity score.
-             The vectors are such that their similarity to the 
-             test vector is above the specified threshold.
-             The list is ordered from larger to smaller 
-             values of the similarity score 
-     
-     NOTES: 
-        * Uses remove-if and sort"
-  
-  )
+		 INPUT:  lst-vectors:   list of vectors
+						 test-vector:   test vector, representad as a list
+						 similarity-fn: reference to a similarity function
+						 threshold:     similarity threshold (default 0)
+			
+		 OUTPUT: list of pairs. Each pair is a list with
+						 a vector and a similarity score.
+						 The vectors are such that their similarity to the 
+						 test vector is above the specified threshold.
+						 The list is ordered from larger to smaller 
+						 values of the similarity score 
+		 
+		 NOTES: 
+				* Uses remove-if and sort"
+	
+	)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -195,47 +199,47 @@ ________________
 
 
 (defun nearest-neighbor (lst-vectors test-vector distance-fn)
-  "Selects from a list the vector that is closest to the 
-   reference vector according to the specified distance function 
+	"Selects from a list the vector that is closest to the 
+	 reference vector according to the specified distance function 
  
-   INPUT:  lst-vectors:   list of vectors
-           ref-vector:    reference vector, represented as a list
-           distance-fn:   reference to a distance function
-      
-   OUTPUT: List formed by two elements:
-           (1) the vector that is closest to the reference vector 
-               according to the specified distance function
-           (2) The corresponding distance value.
+	 INPUT:  lst-vectors:   list of vectors
+					 ref-vector:    reference vector, represented as a list
+					 distance-fn:   reference to a distance function
+			
+	 OUTPUT: List formed by two elements:
+					 (1) the vector that is closest to the reference vector 
+							 according to the specified distance function
+					 (2) The corresponding distance value.
 
 
-   NOTES: 
-      * The implementation is recursive
-      * It ignores the vectors in lst-vectors for which the 
-        distance value cannot be computed."
-  )
+	 NOTES: 
+			* The implementation is recursive
+			* It ignores the vectors in lst-vectors for which the 
+				distance value cannot be computed."
+	)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (defun backward-chaining (goal lst-rules)
-  "Backward-chaining algorithm for propositional logic
+	"Backward-chaining algorithm for propositional logic
  
-   INPUT: goal:      symbol that represents the goal
-          lst-rules: list of pairs of the form 
-                     (<antecedent>  <consequent>)
-                     where <antecedent> is a list of symbols
-                     and  <consequent> is a symbol
+	 INPUT: goal:      symbol that represents the goal
+					lst-rules: list of pairs of the form 
+										 (<antecedent>  <consequent>)
+										 where <antecedent> is a list of symbols
+										 and  <consequent> is a symbol
 
 
-   OUTPUT: T (goal derived) or NIL (goal cannot be derived)
+	 OUTPUT: T (goal derived) or NIL (goal cannot be derived)
 
 
-   NOTES: 
-        * Implemented with some, every" 
+	 NOTES: 
+				* Implemented with some, every" 
 
 
-  (backward-chaining-aux goal lst-rules NIL))
+	(backward-chaining-aux goal lst-rules NIL))
 
 
 
@@ -253,37 +257,37 @@ ________________
 ;;; Breadth-first-search in graphs
 ;;;
 (defun bfs (end queue net)
-  (if (null queue) 
-      NIL
-    (let* ((path (first queue))
-           (node (first path)))
-      (if (eql node end) 
-          (reverse path)
-        (bfs end 
-             (append (rest queue) 
-                     (new-paths path node net)) 
-             net))))) 
+	(if (null queue) 
+			NIL
+		(let* ((path (first queue))
+					 (node (first path)))
+			(if (eql node end) 
+					(reverse path)
+				(bfs end 
+						 (append (rest queue) 
+										 (new-paths path node net)) 
+						 net))))) 
 
 
  (defun new-paths (path node net)
-  (mapcar #'(lambda(n) 
-        (cons n path)) 
-                (rest (assoc node net))))
+	(mapcar #'(lambda(n) 
+				(cons n path)) 
+								(rest (assoc node net))))
 ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 
 (defun shortest-path (start end net)
-  (bfs end (list (list start)) net))    
+	(bfs end (list (list start)) net))    
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
  
 (defun bfs-improved (end queue net)
-  )
+	)
 
 
 
 
 (defun shortest-path-improved (end queue net)
-  )
+	)
